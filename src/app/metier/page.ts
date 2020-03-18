d86f6846a209e14cd1901d9a93f3b23ab2bea90e
ABDELOUAHAD <mustapha.abdelouahad@capgemini.com>
2020-03-18 16:00:58 +0100

    Ajout PAge & Image dans metier

---
+++ src/app/metier/page.ts
@@ -0,0 +1,17 @@
+export class Page<T> {
+
+    constructor(public content: T[],
+                public totalElements: number,
+                // tslint:disable-next-line: variable-name
+                public number: number,
+                public size: number,
+                public totalPages: number,
+                public numberOfElements: number,
+                public first: boolean,
+                public last: boolean,
+                public empty: boolean) {}
+    // fonction utilitaire, genere une page vide du type voulu
+    public static emptyPage<T>(): Page<T> {
+        return new Page<T>([], 0, 0, 5, 1, 0, true, true, true);
+    }
+}
